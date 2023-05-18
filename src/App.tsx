411f2d545c2400f04fd4d08569cdfebf78092f23
Marie Kagermanova <maria.kgm04@gmail.com>
2023-05-18 21:49:49 +0300

    feat: initialize SDK

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import AppExtensionsSDK from "@pipedrive/app-extensions-sdk";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    initializeSDK();
+  });
+
+  async function initializeSDK() {
+    await new AppExtensionsSDK().initialize();
+  }
 
   return (
     <>
@@ -18,18 +27,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
